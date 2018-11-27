c2a4c4cb1b54dc504c91e6ceec4f9833851b779f
DavidPiedra1 <42980864+DavidPiedra1@users.noreply.github.com>
2018-11-27 15:36:12 -0500

    Started on a model

--- letsgo/login/models.py
+++ letsgo/login/models.py
@@ -2,3 +2,6 @@ from django.db import models
 from django.contrib.auth.models import User
 # class UserProfile(models.Model):
 #     Description = models.CharField(max_length=200)
+class Inventory(models.Mobel):
+    Item = models.CharField(max_length=50)
+    Amount = #integer

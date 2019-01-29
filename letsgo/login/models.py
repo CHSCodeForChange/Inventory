7949a1c4a536ed9cd78527416c6a47a5a57ee500
Kishore <pskishore.aug@gmail.com>
2019-01-29 16:00:50 -0500

    New CHange

--- letsgo/login/models.py
+++ letsgo/login/models.py
@@ -2,6 +2,23 @@ from django.db import models
 from django.contrib.auth.models import User
 # class UserProfile(models.Model):
 #     Description = models.CharField(max_length=200)
-class Inventory(models.Mobel):
-    Item = models.CharField(max_length=50)
-    Amount = #integer
+class Inventory(models.Model):
+    owner = models.ForeignKey(
+        User,
+        on_delete=models.CASCADE,
+        null  = True,
+        blank = True,
+    )
+    title = models.CharField(max_length=100)
+    description = models.CharField(max_length=100)
+    
+
+class InventoryItem(models.Model):
+    parentInventory = models.ForeignKey(
+        Inventory,
+        on_delete=models.CASCADE,
+        null  = True,
+        blank = True,
+    )
+    title = models.CharField(max_length=100)
+    description = models.CharField(max_length=200)
